72d01568cdd5438cfe4d3b4c43ffa9386d9b18e1
shidel <shidel@users.noreply.github.com>
2022-08-20 08:13:56 -0400

    devel

--- SOURCE/FDIMPLES/build.sh
+++ SOURCE/FDIMPLES/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-rm FDIMPLES.COM >/dev/null
-nasm "FDIMPLES.ASM" -ILIBS/ -fbin -O9 -o "FDIMPLES.COM" || exit 1
-if [ ! -f FDIMPLES.COM ] ; then
-	exit 1
-fi;
-if [ ! -d ../../BIN ] ; then
-	mkdir ../../BIN || exit 1
-fi
-if [ -f ../../BIN/FDIMPLES.COM ] ; then
-	ls -al ../../BIN/FDIMPLES.COM
-fi
-cp FDIMPLES.COM ../../BIN/FDIMPLES.COM
-ls -al ../../BIN/FDIMPLES.COM
+function build () {
 
+	rm "${1}.COM" 2>&1 >/dev/null
+	nasm "${1}.ASM" -ILIBS/ -fbin -O9 -o "${1}.COM" || exit 1
+	if [ ! -f  "${1}.COM" ] ; then
+		exit 1
+	fi;
+	if [ ! -d ../../BIN ] ; then
+		mkdir ../../BIN || exit 1
+	fi
+	if [ -f "../../BIN/${1}.COM" ] ; then
+		ls -al "../../BIN/${1}.COM"
+	fi
+	cp "${1}.COM" "../../BIN/${1}.COM"
+	ls -al "../../BIN/${1}.COM"
+}
+
+build FDIMPLES
